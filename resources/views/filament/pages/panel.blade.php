e9e14fe354ac3b5a742b9aeeca1abccbb9db673d
Carlos Sosa <carlosupremedev@gmail.com>
2025-02-22 09:52:06 -0600

    Feat: Using widgets from alerts by team

--- resources/views/filament/pages/panel.blade.php
+++ resources/views/filament/pages/panel.blade.php
@@ -4,4 +4,8 @@
     <livewire:equipment-check-stats/>
 
     <livewire:area-reports-chart/>
+
+    @livewire(App\Filament\Widgets\EquipmentAlertsWidget::class)
+
+    @livewire(App\Filament\Widgets\EquipmentAlertsChart::class)
 </x-filament-panels::page>
