19e3e0305a9bfd98a363a519fcfff0f2d0a0fd28
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2025-04-04 12:34:29 -0600

    Fix: Optimizing Alpine deps

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
 import Alpine from 'alpinejs'
-import focus from 'alpinejs/focus'
-import collapse from 'alpinejs/collapse'
+import focus from '@alpinejs/focus'
+import collapse from '@alpinejs/collapse'
  
 Alpine.plugin(collapse)
 Alpine.plugin(focus)
