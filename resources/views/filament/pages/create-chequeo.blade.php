7752e97de38bd639adf5dd7070599995a506967a
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2026-01-23 18:20:31 -0600

    Fix: Simplifying the logic on Create-chequeo view

--- resources/views/filament/pages/create-chequeo.blade.php
+++ resources/views/filament/pages/create-chequeo.blade.php
@@ -1,6 +1,42 @@
 <x-filament-panels::page>
     {{-- Top Header Section --}}
-    @if(!$hojaChequeo)
+    @if($hojaChequeo && $this->hasItems())
+        <div class="flex sm:flex-row flex-col gap-4 items-center w-full mb-6 mt-3 place-content-center">
+            <button wire:click="resetState"
+                    class="flex items-center  px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 dark:hover:bg-gray-700">
+                @svg('heroicon-o-arrow-left', 'w-4 h-4 mr-4')
+                Regresar
+            </button>
+            <div class="flex gap-4 items-center place-content-center flex-wrap">
+                <h2 class="font-bold">{{ $this->hojaChequeo->equipo->nombre }}</h2>
+                <x-filament::badge color="primary">{{$this->hojaChequeo->equipo->tag}}</x-filament::badge>
+                <x-filament::badge color="primary">{{$user->turno->nombre}}</x-filament::badge>
+                @if($this->hojaEjecucion)
+                    <x-filament::badge color="primary">Reanudando</x-filament::badge>
+                @endif
+            </div>
+
+            {{$this->dateForm}}
+        </div>
+
+        <livewire:chequeo-items :hoja="$hojaChequeo" :ejecucion="$hojaEjecucion"/>
+
+        @if($this->hojaChequeo->observaciones)
+            <div>
+                <h2 class="font-bold">Observaciones:</h2>
+                {!! $this->hojaChequeo->observaciones !!}
+            </div>
+        @endif
+
+        <form>
+            {{ $this->form }}
+        </form>
+
+        <div class="flex w-full gap-2 my-5 items-center place-content-end">
+            <x-filament::button wire:click="create" class="mt-4">Guardar</x-filament::button>
+        </div>
+        <x-filament-actions::modals/>
+    @else
         <div data-page="create-chequeo"
              class="flex flex-col md:flex-row justify-between items-start md:items-center gap-6">
             <div class="space-y-1">
@@ -58,66 +94,13 @@
                 </div>
             </div>
         </div>
-    @endif
-
-    @if(!$hojaChequeo)
         <livewire:select-hoja-chequeo/>
-    @else
-        <div class="flex sm:flex-row flex-col gap-4 items-center w-full mb-6 mt-3 place-content-center">
-            <button wire:click="resetState"
-                    class="flex items-center  px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 dark:hover:bg-gray-700">
-                @svg('heroicon-o-arrow-left', 'w-4 h-4 mr-4')
-                Regresar
-            </button>
-            <div class="flex gap-4 items-center place-content-center flex-wrap">
-                <h2 class="font-bold">{{ $this->hojaChequeo->equipo->nombre }}</h2>
-                <x-filament::badge color="primary">{{$this->hojaChequeo->equipo->tag}}</x-filament::badge>
-                <x-filament::badge color="primary">{{$user->turno->nombre}}</x-filament::badge>
-                @if($this->hojaEjecucion)
-                    <x-filament::badge color="primary">Reanudando</x-filament::badge>
-                @endif
-            </div>
-
-            {{$this->dateForm}}
-        </div>
-
-        @if($this->hasItems())
-            <livewire:chequeo-items :hoja="$hojaChequeo" :ejecucion="$hojaEjecucion"/>
-
-            @if($this->hojaChequeo->observaciones)
-                <div>
-                    <h2 class="font-bold">Observaciones:</h2>
-                    {!! $this->hojaChequeo->observaciones !!}
-                </div>
-            @endif
-
-            <form>
-                {{ $this->form }}
-            </form>
-
-            <div class="mt-4">
-                @error('items')
-                <span class="text-red-500">{{ $message }}</span>
-                @enderror
-            </div>
-
-            <div class="flex w-full gap-2 my-5 items-center place-content-end">
-                <x-filament::button wire:click="create" class="mt-4">Guardar</x-filament::button>
-            </div>
-            <x-filament-actions::modals/>
-        @else
-            <div class="bg-gray-50 gap-4 w-full grid place-items-center p-4 mb-4">
-                <h2 class="font-bold text-slate-600">No hay items en esta hoja de chequeo</h2>
-                <x-filament::button wire:click="resetState" color="gray" class="mt-4">Elegir otra
-                </x-filament::button>
-            </div>
-        @endif
     @endif
-<script>
-    document.addEventListener('livewire:init', () => {
-        Livewire.on('scroll-to-top', () => {
-            window.scrollTo({top: 0, behavior: 'smooth'});
+    <script>
+        document.addEventListener('livewire:init', () => {
+            Livewire.on('scroll-to-top', () => {
+                window.scrollTo({top: 0, behavior: 'smooth'});
+            });
         });
-    });
-</script>
+    </script>
 </x-filament-panels::page>
