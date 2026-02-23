acb1d1426c243f1eed2ae24b0dbf20f6e264344a
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2026-02-23 11:53:56 -0600

    feat: reporte filters table estado

--- app/Filament/Resources/Reportes/Pages/ListReportes.php
+++ app/Filament/Resources/Reportes/Pages/ListReportes.php
@@ -5,10 +5,27 @@ namespace App\Filament\Resources\Reportes\Pages;
 use App\Filament\Resources\Reportes\ReporteResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListReportes extends ListRecords
 {
     protected static string $resource = ReporteResource::class;
+
+    public function getTabs(): array
+    {
+        $tabs = [
+            'all' => Tab::make('Todos')
+                ->icon('heroicon-m-list-bullet'),
+        ];
+
+        foreach (['pendiente', 'realizado'] as $cc) {
+            $tabs['cc_'.$cc] = Tab::make(ucfirst($cc))
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('estado', $cc));
+        }
+
+        return $tabs;
+    }
 
     protected function getHeaderActions(): array
     {
