8ec80af33b119a98294b9a91232d86a573a4e69c
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2026-09-09 11:32:35 -0600

    fix(redis): soportar predis como cliente de redis para entorno de desarrollo

--- config/database.php
+++ config/database.php
@@ -144,7 +144,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => extension_loaded('redis') ? env('REDIS_CLIENT', 'phpredis') : 'predis',
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
